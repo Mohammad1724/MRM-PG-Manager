710d5f16f4a3aeba8aa50ff6065e7a7499d28d27
Mohammad1724 <129998091+Mohammad1724@users.noreply.github.com>
2026-07-21 17:10:59 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 2
-        versionName = "0.2.0"
+        versionName = "0.2.3"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
